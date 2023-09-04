201684f80f257f46df042b32f26d0a5445e0087a
Evis Drenova <evis@usenucleus.cloud>
2023-09-03 17:56:29 -0700

    ability to change background color and bg icon color

--- app/page.tsx
+++ app/page.tsx
@@ -30,14 +30,19 @@ import {
   PlusIcon,
   GridIcon,
 } from "@radix-ui/react-icons";
+import { ChromePicker, SketchPicker } from "react-color";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Input } from "@/components/ui/input";
 
 const nodeTypes: NodeTypes = {
   roundedBox: RoundedBox,
   circle: Circle,
   square: Square,
 };
-
-type BgVariant = "dots" | "cross" | "grid";
 
 export default function Home() {
   const [nodes, setNodes] = useState<Node[]>([]);
@@ -46,6 +51,8 @@ export default function Home() {
   const [bgVariant, setBgVariant] = useState<BackgroundVariant>(
     BackgroundVariant.Dots
   );
+  const [bgColor, setBgColor] = useState<string>("#F6F6F6");
+  const [bgIconColor, setBgIconColor] = useState<string>("#DADADA");
 
   const AddCircle = () => {
     const currNodes = nodes;
@@ -96,6 +103,13 @@ export default function Home() {
     [setEdges]
   );
 
+  const handleBgIconColorChange = (color: any) => {
+    setBgIconColor(color.hex);
+  };
+
+  const handleBgColorChange = (color: any) => {
+    setBgColor(color.hex);
+  };
   return (
     <div className="flex flex-row w-full">
       <div
@@ -111,6 +125,24 @@ export default function Home() {
         <Button variant="outline" onClick={AddCircle}>
           <CircleIcon />
         </Button>
+        <div
+          className="flex flex-row space-x-2 items-center"
+          id="bg-color-setter"
+        >
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                className={`w-[40px] h-[40px] border-2 border-gray-400 bg-[${bgColor}]`}
+                variant="ghost"
+                onClick={() => setBgColor(bgColor)}
+              />
+            </PopoverTrigger>
+            <PopoverContent>
+              <ChromePicker color={bgColor} onChange={handleBgColorChange} />
+            </PopoverContent>
+          </Popover>
+          <div id="color-name text-gray-800">{bgColor}</div>
+        </div>
         <div className="flex flex-col pt-5">
           <div className="text-gray-600 text-sm">Background</div>
           <div className="flex flex-row space-x-1 pt-2">
@@ -132,6 +164,29 @@ export default function Home() {
             >
               <GridIcon />
             </Button>
+            <Input type="size" className="w-[24px]" />
+          </div>
+          <div
+            className="flex flex-row space-x-2 items-center"
+            id="bg-icon-color-setter"
+          >
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  className={`w-[40px] h-[40px] border-2 border-gray-400`}
+                  style={{ backgroundColor: `${bgIconColor}` }}
+                  variant="ghost"
+                  onClick={() => setBgColor(bgIconColor)}
+                />
+              </PopoverTrigger>
+              <PopoverContent>
+                <ChromePicker
+                  color={bgIconColor}
+                  onChange={handleBgIconColorChange}
+                />
+              </PopoverContent>
+            </Popover>
+            <div id="color-name text-gray-800">{bgIconColor}</div>
           </div>
         </div>
       </div>
@@ -143,6 +198,8 @@ export default function Home() {
         onConnect={onConnect}
         nodeTypes={nodeTypes}
         bgVariant={bgVariant}
+        bgColor={bgColor}
+        bgIconColor={bgIconColor}
       />
     </div>
   );
@@ -156,6 +213,8 @@ interface FlowProps {
   onConnect: OnConnect;
   nodeTypes: NodeTypes;
   bgVariant: BackgroundVariant;
+  bgColor: string;
+  bgIconColor: string;
 }
 
 function ReactFlowCanvas(props: FlowProps): ReactElement {
@@ -167,6 +226,8 @@ function ReactFlowCanvas(props: FlowProps): ReactElement {
     onConnect,
     nodeTypes,
     bgVariant,
+    bgColor,
+    bgIconColor,
   } = props;
   const fitViewOptions: FitViewOptions = {
     padding: 10,
@@ -174,7 +235,6 @@ function ReactFlowCanvas(props: FlowProps): ReactElement {
   const defaultEdgeOptions: DefaultEdgeOptions = {
     animated: true,
   };
-
   return (
     <div className="w-full h-[720px]">
       <ReactFlow
@@ -188,7 +248,12 @@ function ReactFlowCanvas(props: FlowProps): ReactElement {
         fitView
         fitViewOptions={fitViewOptions}
       >
-        <Background className="bg-gray-100" variant={bgVariant} size={2} />
+        <Background
+          style={{ backgroundColor: `${bgColor}` }}
+          color={bgIconColor}
+          variant={bgVariant}
+          size={2}
+        />
         <Controls />
         <MiniMap />
       </ReactFlow>
